bf0841f8ff4d2c602238a670878018f5fcb82bed
phoenixjmh <phoenixjmh@gmail.com>
2023-03-22 16:58:44 -0600

    Initialized

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+module.exports = {
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader","sass-loader"],
+      },
+      
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: "asset/resource",
+      },
+    ],
+  },
+};

41e39c78b2089cd7834f6694801fd8d5b553bee5
phoenixjmh <phoenixjmh@gmail.com>
2023-03-24 13:27:10 -0600

    Todos can now be dragged and dropped into a new project. Ugly implementation, clean up in a bit.

--- src/scripts/drawProjectsToList.js
+++ src/scripts/drawProjectsToList.js
@@ -1,6 +1,7 @@
 import createNewTodoButton from "./newTodoButton";
 import drawExistingTodos from "./drawExistingTodos";
 import removeElement from "./removeElement";
+import Todo from "./Todo";
 
 const drawProjectsToList = (
   pm,
@@ -56,6 +57,26 @@ const drawProjectsToList = (
     1;
     drawExistingTodos(pm, project, displayPanel);
     let newFormButton = createNewTodoButton(pm,project, displayPanel);
+  });
+  projectDiv.addEventListener('dragover',(event)=>{
+    event.preventDefault();
+  });
+  projectDiv.addEventListener('drop',(event)=>{
+    event.preventDefault();
+    console.log('Drop on',project.title);
+    const todoToAdd=JSON.parse(event.dataTransfer.getData("text")).todoObj;
+    const sourceProject=JSON.parse(event.dataTransfer.getData('text')).projectObj;
+    console.log('Adding',todoToAdd,"to",project.title,'from',sourceProject);
+    const todo = new Todo(todoToAdd._title,todoToAdd._description,todoToAdd._dueDate,todoToAdd._id);
+    project.addTodo(todo);
+    let arr=pm.getAll();
+    for (let p in arr){
+      if(arr[p].id==sourceProject._id){
+        let projRef=arr[p];
+        removeElement(projRef,todo.id);
+      }
+    }
+    localStorage.setItem('packageManager',JSON.stringify(pm));
   });
   projectDiv.appendChild(projectTitleH1);
   projectDiv.appendChild(editButton);

7742b7d3037b828946a4179cb428faa26bdbcea4
phoenixjmh <phoenixjmh@gmail.com>
2023-03-29 13:30:26 -0600

    Default project glitch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import ProjectManager from "./modules/ProjectManager";
 import Todo from "./modules/Todo";
 import Project from "./modules/Project";
@@ -5,45 +6,33 @@ import {
   drawProjectManager,
   drawProjectDisplayPanel,
 } from "./modules/loadMain";
-import drawProjectsOnLoad from "./modules/loadProjects";
 import "./main.scss";
 import loadProjects from "./modules/loadProjects";
 import saveWork from "./modules/Storage";
+
 let savedProjects;
-let pm = new ProjectManager();
-if(localStorage.getItem("packageManager")!=null){
-
-    savedProjects = JSON.parse(localStorage.getItem("packageManager")).projects;
-    savedProjects.forEach((item,index)=>{
-        let proj=new Project(item._title,item._id,item._div);
-        pm.addProj(proj);
-        let todos=item.todos;
-        todos.forEach((todo)=>{
-            console.log(todo);
-            let tempTodo=new Todo(todo._title,todo._dueDate,todo._id);
-            proj.addTodo(tempTodo);
-        })
-        
-    })
-    if(pm.getAll().length<1)
-    {
-        let defaultProject=new Project('All',0);
-    pm.addProj(defaultProject);
-    }
+const pm = new ProjectManager();
+if (localStorage.getItem("packageManager") != null) {
+  savedProjects = JSON.parse(localStorage.getItem("packageManager")).projects;
+  savedProjects.forEach((item) => {
+    const proj = new Project(item._title, item._id, item._div);
+    pm.addProj(proj);
+    const { todos } = item;
+    todos.forEach((todo) => {
+      console.log(todo);
+      const tempTodo = new Todo(todo._title, todo._dueDate, todo._id);
+      proj.addTodo(tempTodo);
+    });
+  });
+} else if (pm.getAll().length < 1) {
+  const defaultProject = new Project("All", 0);
+  pm.addProj(defaultProject);
 }
-
-
-
-
 
 const displayPanel = drawProjectDisplayPanel();
 const projectManagerDiv = drawProjectManager(pm, displayPanel);
 
-//Development set up stuff
-
+// Development set up stuff
 
 loadProjects(pm, projectManagerDiv, displayPanel);
 saveWork(pm);
-
-
-
